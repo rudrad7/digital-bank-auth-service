02cb65a823a29d0741ead582bf4adf0d59376c4b
Davare <maharudra-vitthal.davare@capgemini.com>
2024-07-08 15:43:00 +0530

    first commit

---
+++ src/main/java/com/db/DigitalBankAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankAuthServiceApplication.class, args);
+	}
+
+}
